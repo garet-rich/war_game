9d9e1658159b06218c650526ce0b028d490cb7ad
garet-rich <garet.rich@gmail.com>
2021-04-28 12:40:51 -0600

    Created deck functions. Created card_test functions

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,3 +1,46 @@
-class Deck()
-    
+require_relative 'card'
+
+class Deck
+    def initialize(cards)
+        @cards = cards
+    end
+
+    def cards()
+        return @cards
+    end
+
+    def rank_of_card_at(index)
+        return @cards[index].value
+    end
+
+    def high_ranking_cards()
+        high_cards = []
+
+        @cards.each do |item|
+            if item.value >= 11
+                high_cards << (item)
+            end
+        end
+
+        return high_cards
+    end
+
+    def percent_high_ranking()
+        high_card_counter = 0
+
+        @cards.each do |item|
+            if item.value >= 11
+                high_card_counter += 1
+            end
+        end
+        return (high_card_counter.to_f / @cards.length.to_f).round(4)*100
+    end
+
+    def remove_card()
+        @cards.shift()
+    end
+
+    def add_card(card)
+        @cards << card
+    end
 end
