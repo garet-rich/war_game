be52a7d23d6894524224436d846cd77a2e48324d
garet-rich <garet.rich@gmail.com>
2021-04-28 11:08:41 -0600

    Created reader methods for Card class

--- lib/card.rb
+++ lib/card.rb
@@ -1,3 +1,21 @@
-class Card()
-    
+class Card
+    def initialize(suit, rank, value)
+        @suit = suit
+        @rank = rank
+        @value = value
+    end
+
+    def suit()
+        return @suit
+    end
+
+    def rank()
+        return @rank
+    end
+
+    def value()
+        return @value
+    end
 end
+
+test1 = Card.new(:heart, 'Jack', 11)
