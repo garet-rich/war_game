20632bdd27514c6c9fc2b2e5405253214b55f7cc
garet-rich <garet.rich@gmail.com>
2021-05-05 15:06:44 -0600

    Commented out test code to be moved into testing files

--- lib/player.rb
+++ lib/player.rb
@@ -24,13 +24,13 @@ class Player
     end
 end
 
-card1 = Card.new(:diamond, 'Queen', 12)
-card2 = Card.new(:spade, '3', 3)    
-card3 = Card.new(:heart, 'Ace', 14)    
-deck = Deck.new([card1, card2, card3])
-player = Player.new('Clarisa', deck)
-player.name
-player.deck
-player.has_lost?
-player.deck.remove_card
-player.has_lost?
+# card1 = Card.new(:diamond, 'Queen', 12)
+# card2 = Card.new(:spade, '3', 3)    
+# card3 = Card.new(:heart, 'Ace', 14)    
+# deck = Deck.new([card1, card2, card3])
+# player = Player.new('Clarisa', deck)
+# player.name
+# player.deck
+# player.has_lost?
+# player.deck.remove_card
+# player.has_lost?
